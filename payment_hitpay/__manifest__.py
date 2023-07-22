498cab97983c15e7a32d98b4701b3fd0f592cb0a
PRABU KUPPUSAMY <kulance@gmail.com>
2023-07-22 12:07:39 +0530

    Initial Version

---
+++ payment_hitpay/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "HitPay Payment Gateway",
+    'version': '1.0',
+    'category': 'Accounting/Payment Providers',
+    'sequence': 350,
+    'summary': "HitPay Payment Gateway Plugin allows merchants to accept PayNow QR, Cards, Apple Pay, Google Pay, WeChatPay, AliPay and GrabPay Payments.",
+    'author': 'HitPay Payment Solutions Pte Ltd',
+    'website': 'https://www.hitpayapp.com',
+    'depends': ['payment'],
+    'data': [
+        'views/payment_hitpay_templates.xml',
+        'views/payment_provider_views.xml',
+		'data/payment_icon_data.xml',
+        'data/payment_provider_data.xml',
+    ],
+    'images': ['static/description/logo.png'],
+    'application': True,
+    'installable': True,
+    'license': 'LGPL-3',
+}
