e8f650ebaabcaf0e4cf3eb5c358d3d7031238834
PRABU KUPPUSAMY <kulance@gmail.com>
2024-06-26 00:31:05 +0530

    pos_hitpay removed from 17.0 version since it is not available for 17.0 yet

--- pos_hitpay/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from ..utils import error_handler
-
-from . import hitpaypos_client
-from . import pos_payment_method
-from . import pos_order
-from . import pos_payment
-from . import pos_session
